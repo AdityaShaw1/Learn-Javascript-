4eb38496307f616ecea939771475da61f7455c21
Aditya Shaw <107760186+AdityaShaw1@users.noreply.github.com>
2024-02-05 18:00:00 +0000

    Added JS variable file

---
+++ Basics/Variables.js
@@ -0,0 +1,5 @@
+const accountId = 12365
+let accountEmail = "shawaditya382@gmail.com"
+var accountPassword = "12354"
+accountCity = "Asansol"
+console.log(accountId,accountEmail,accountPassword,accountCity)
